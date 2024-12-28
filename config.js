ac9f7219ce059ff0aae472027395d6f66cc6cdb0
Yusuf-Amr <yusufamr414@gmail.com>
2024-12-28 05:59:56 +0200

    Initial commit

---
+++ config.js
@@ -0,0 +1,59 @@
+window.PorfolioConfig = {
+  name: 'Yusuf Amr',
+  jobTitle: 'Cybersecurity Analyst',
+  jobDescription: 'On a journey to learn and share insights within the cybersecurity community.',
+  article: [
+    {
+      name: 'Email Generator Tool',
+      link: 'articles/email_generator_tool.html',
+      description: 'I wanted to share a tool that I’ve created and really enjoy using. It’s not highly technical,...'
+    },
+    {
+      name: 'Email Sender Validation Tool',
+      link: 'articles/email_sender_validation_tool.html',
+      description: 'Excited to share a tool I built to simplify part of the email analysis for security analysts! This tool...'
+    },
+    {
+      name: 'AWS Breach Hunting: Investigating Security Incidents in the Cloud with AWS CloudTrail (Pwned Labs)',
+      link: 'articles/aws_breach_hunting.html',
+      description: 'Hey there! I’m Yusuf Amr, your go-to cybersecurity analyst and a certified AWS Cloud Solutions...'
+    },
+    {
+      name: 'Deciding Your Cybersecurity Future: Recognizing When It’s Not for You',
+      link: 'articles/desciding_cybersecurity_future.html',
+      description: 'Is Cybersecurity the Right Path for You? Choosing a career in cybersecurity is a major decision that...'
+    },
+    {
+      name: 'Walkthrough: LGDroid Android Forensics Lab',
+      link: 'articles/LGDroid_Android_Forensics_Lab.html',
+      description: 'Given Scenario: “Our IR team took a disk dump of the android phone. As a soc analyst, analyze...'
+    },
+    {
+      name: 'Beyond the Walkthoughs: Building a Strategic Approach to Blue Team Labs',
+      link: 'articles/Building_Strategic_Approach_Blue_Team_Labs.html',
+      description: 'In this article, I want to share a tip strategy for tackling cybersecurity labs effectively. We’ve...'
+    },
+    {
+      name: 'Malware Analysis Report on LockBit 3.0 Ransomware',
+      link: 'articles/lockbit_report.html',
+      description: 'This is an analysis of malware for the LockBit 3.0 ransomware sample that was discovered on February...'
+    },
+    {
+      name: 'Private Local AI',
+      link: 'articles/private_ai.html',
+      description: 'I’ve often been concerned about the accidental leakage of sensitive information through AI tools...'
+    }
+  ],
+  contacts: {
+    links: [
+      {
+        name: 'Linkedin',
+        link: 'https://www.linkedin.com/in/yusuf-amr/'
+      },
+      {
+        name: 'GitHub',
+        link: 'https://github.com/Yusuf-Amr'
+      }
+    ]
+  }
+};
